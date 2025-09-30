fcb3ee513e44bba5c6bd2dd4735d77d75390e2e9
Pontain <pontus.orstadius@gmail.com>
2025-09-30 12:49:16 +0200

    Version 1

--- PYAI25_Grupp-vning1-main/student.py
+++ PYAI25_Grupp-vning1-main/student.py
@@ -2,6 +2,7 @@ import json
 
 # Här ska alla studenter finnas! 
 # key = student id, value = dictionary student [se nedan]
+
 students = {}
 course_list = ["math", "physics", "chemistry"]
 
@@ -15,6 +16,7 @@ course_list = ["math", "physics", "chemistry"]
 ##########################
 
 ##############################################################################################################
+
 
 def create_new_student1(student_id, name, age):
 
@@ -32,7 +34,8 @@ def create_new_student1(student_id, name, age):
     
 ##############################################################################################################
 
-def add_course(student_id, course):
+
+def add_course1(student_id, course):
 
     if student_id not in students:
         return 1
@@ -51,7 +54,8 @@ def add_course(student_id, course):
     
 ##############################################################################################################
 
-def remove_course(student_id, course):
+
+def remove_course1(student_id, course):
     if student_id not in students:
         return 1
     
@@ -66,6 +70,7 @@ def remove_course(student_id, course):
     
 ##############################################################################################################
 
+
 def get_student_name(student_id):
 
     if student_id in students:
@@ -76,6 +81,7 @@ def get_student_name(student_id):
     # givet korrekt student_id så returnerar vi namnet
 
 ############################################################################################################## 
+
 
 def get_student_age(student_id):
 
@@ -88,6 +94,7 @@ def get_student_age(student_id):
     
 ##############################################################################################################
 
+
 def get_student_courses(student_id):
 
     if student_id in students:
@@ -99,6 +106,7 @@ def get_student_courses(student_id):
 
 ##############################################################################################################
 
+
 def save_students():
 
     with open("students.json", "w", encoding="utf-8") as f:
@@ -108,6 +116,7 @@ def save_students():
 
 ##############################################################################################################
 
+
 def read_students():
 
     global students
@@ -116,8 +125,9 @@ def read_students():
         with open("students.json", "r", encoding="utf-8") as f:
             students = json.load(f)
 
-            students = {int(id): student_data for id, student_data in students.items()} #Konverterar tillbaka id-nyckeln till int från str
-                                                                                        # då json sparar allt som str.
+            students = {int(id): student_data for id, student_data in students.items()} 
+
+            #Konverterar tillbaka id-nyckeln till int från str då json sparar allt som str.
 
     except FileNotFoundError:
         print("Ingen fil hittad.")
@@ -126,6 +136,7 @@ def read_students():
     # Här ska vi läsa in sparade studenter och spara i vår dictionary "students"
 
 ##############################################################################################################
+
 
 def create_new_student():
 
@@ -139,6 +150,7 @@ def create_new_student():
 
         if create_new_student1(student_id, name, age):
             print(f"Student {name} with id {student_id} was added.")
+            save_students()
             input("Press enter to continue.")
 
         else:
@@ -149,119 +161,118 @@ def create_new_student():
 
 ############################################ Add course ############################################################
 
-    if val == "2":
 
-        try:
+def add_course():    
+
+    try:
+        
+        student_id = int(input("Student id: "))
+        course = input("Course: ").lower()
+
+        output = add_course1(student_id, course)
+
+        # Här händer följande:
+
+        # Funktionen add_course anropas med de två argumenten student_id och courses
+        # Funktionen kör sin kod
+        # Den kollar om student-id finns i students
+        # Den kollar om kursen redan finns i studentens "courses"-lista
+        # Den kollar ifall kursen finns i kurslistan.
+        # Om allt är okej → kursen läggs till i listan
+        # Sedan returnerar funktionen en statuskod (t.ex. 0, 1, 2, 3)
+        # Returnvärdet sparas i variabeln output.
+        # Om funktionen returnerar 0, så blir output = 0
+        # Om den returnerar 1, så blir output = 1, osv.
+
+        # Menyn använder output för att avgöra vilket meddelande som ska skrivas ut.
+
+        if output == 0:
+            print(f"{course} added for {get_student_name(student_id)}")
+            save_students()
+            input("Press enter to continue.")
             
-            student_id = int(input("Student id: "))
-            course = input("Course: ").lower()
+        elif output == 1:
+            print("Wrong id, student not registred.")
 
-            output = add_course(student_id, course)
+        elif output == 2:
+            print("Course already added.")
+        
+        elif output == 3:
+            print("Course not avalible.")
 
-            # Här händer följande:
-
-            # Funktionen add_course anropas med de två argumenten student_id och courses
-            # Funktionen kör sin kod
-            # Den kollar om student-id finns i students
-            # Den kollar om kursen redan finns i studentens "courses"-lista
-            # Den kollar ifall kursen finns i kurslistan.
-            # Om allt är okej → kursen läggs till i listan
-            # Sedan returnerar funktionen en statuskod (t.ex. 0, 1, 2, 3)
-            # Returnvärdet sparas i variabeln output.
-            # Om funktionen returnerar 0, så blir output = 0
-            # Om den returnerar 1, så blir output = 1, osv.
-
-            # Menyn använder output för att avgöra vilket meddelande som ska skrivas ut.
-
-            if output == 0:
-                print(f"{course} added for {get_student_name(student_id)}")
-                input("Press enter to continue.")
-
-
-            elif output == 1:
-                print("Wrong id, student not registred.")
-
-            elif output == 2:
-                print("Course already added.")
-            
-            elif output == 3:
-                print("Course not avalible.")
-
-        except ValueError:
-            print("Invalid registration, id = digits, courses = text.")
-
-        #input("Press enter to continue.")
-
+    except ValueError:
+        print("Invalid registration, id = digits, courses = text.")
 
 ############################################ Remove course ############################################################
 
-    if val == "3":
 
-        try:
+def remove_course():
 
-            student_id = int(input("Student id: "))
-            course = input("Course to remove: ")
+    try:
 
-            output = remove_course(student_id, course)
+        student_id = int(input("Student id: "))
+        course = input("Course to remove: ")
 
-            if output == 0:
-                print(f"{course} removed from student {get_student_name(student_id)}")
-                input("Press enter to continue.")
+        output = remove_course1(student_id, course)
 
-            elif output == 1:
-                print("Wrong id, student not registred.")
+        if output == 0:
+            print(f"{course} removed from student {get_student_name(student_id)}")
+            save_students()
+            input("Press enter to continue.")
 
-            elif output == 2:
-                print("Course not registred with student.")
+        elif output == 1:
+            print("Wrong id, student not registred.")
 
-        except ValueError:
-            print("Invalid registration, id = digits, courses = text.")
+        elif output == 2:
+            print("Course not registred with student.")
 
-    #input("Press enter to continue.")
-
+    except ValueError:
+        print("Invalid registration, id = digits, courses = text.")
 
 ############################################ Show a student ############################################################
 
-    if val == "4":
 
-        try:
+def show_student():
 
-            student_id = int(input("Student id: "))
+    try:
 
-            if student_id in students:
-                
-                print(f"id: {student_id}")
-                print(f"Name: {get_student_name(student_id)}")
-                print(f"Age: {get_student_age(student_id)}")
-                if students[student_id]["courses"]:
-                    print(f"Courses: {', '.join(get_student_courses(student_id))}") # .join lägger ihop raderna i loopen efter varandra separerat med det skrivet framför .join
-                else:
-                    print("No courses registred.")
+        student_id = int(input("Student id: "))
 
+        if student_id in students:
+            
+            print(f"id: {student_id}")
+            print(f"Name: {get_student_name(student_id)}")
+            print(f"Age: {get_student_age(student_id)}")
+            if students[student_id]["courses"]:
+                print(f"Courses: {', '.join(get_student_courses(student_id))}") # .join lägger ihop raderna i loopen efter varandra separerat med det skrivet framför .join
             else:
-                print("Student not registred.")
-        except ValueError:
-            print("Invalid input, only digits in id-number")
-    #input("Press enter to continue.")
-    
+                print("No courses registred.")
+            input("Press enter to continue.")
+        else:
+            print("Student not registred.")
+
+    except ValueError:
+        print("Invalid input, only digits in id-number")
 
 ############################################ Show all students ############################################################
 
-    if val == "5":
 
-        print("~" * (längst_rad + 4))
+def show_all_students():
 
-        for student_id in students:
-                print(f"id: {student_id}")
-                print(f"Name: {get_student_name(student_id)}")
-                print(f"Age: {get_student_age(student_id)}")
-                if students[student_id]["courses"]:
-                    print(f"Courses: {', '.join(get_student_courses(student_id))}") # .join lägger ihop raderna i loopen efter varandra separerat med det skrivet framför .join
-                else:
-                    print("No courses registred.")
-                print("~" * (längst_rad + 4))
-        input("Press enter to continue.")
+    print("~" * 30)
 
+    for student_id in students:
+            print(f"id: {student_id}")
+            print(f"Name: {get_student_name(student_id)}")
+            print(f"Age: {get_student_age(student_id)}")
+            if students[student_id]["courses"]:
+                print(f"Courses: {', '.join(get_student_courses(student_id))}") # .join lägger ihop raderna i loopen efter varandra separerat med det skrivet framför .join
+            else:
+                print("No courses registred.")
+            print("~" * 30)
+            input("Press enter to continue.")
+
+########################################################################################################
 
 
 
